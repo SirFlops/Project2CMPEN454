bdb9a649daa9922bce4c3b1b4c4d1807bc088caa
Evanyoung5 <112011642+Evanyoung5@users.noreply.github.com>
2024-04-01 14:59:45 -0400

    Continued working on world to camera transformations

--- Project2DataFiles/Project2DataFiles/Project2CMPEN454.m
+++ Project2DataFiles/Project2DataFiles/Project2CMPEN454.m
@@ -2,18 +2,21 @@ ParamOneObject = matfile('Parameters_V1.mat');
 ParamTwoObject = matfile('Parameters_V2.mat');
 
 
-positionOne = getfield(ParamOneObject.Parameters(1,1),'position')
-rotationOne = getfield(ParamOneObject.Parameters(1,1),'Rmat')
-KmatrixOne = getfield(ParamOneObject.Parameters(1,1),'Kmat')
+positionOne = getfield(ParamOneObject.Parameters(1,1),'position');
+rotationOne = getfield(ParamOneObject.Parameters(1,1),'Rmat');
+KmatrixOne = getfield(ParamOneObject.Parameters(1,1),'Kmat');
 SMatrix = [1 0 0 -positionOne(1);0 1 0 -positionOne(2); 0 0 1 -positionOne(3); 0 0 0 1];
-foclength = getfield(ParamOneObject.Parameters(1,1),'foclen')
-RMatrix = [rotationOne(1,1) rotationOne(1,2) rotationOne(1,3) 0;rotationOne(2,1) rotationOne(2,2) rotationOne(2,3) 0;rotationOne(3,1) rotationOne(3,2) rotationOne(3,3) 0;0 0 0 1]
-FocalMatrix = [foclength 0 0 0; 0 foclength 0 0;0 0 1 0]
-
-
+foclength = getfield(ParamOneObject.Parameters(1,1),'foclen');
+RMatrix = [rotationOne(1,1) rotationOne(1,2) rotationOne(1,3) 0;rotationOne(2,1) rotationOne(2,2) rotationOne(2,3) 0;rotationOne(3,1) rotationOne(3,2) rotationOne(3,3) 0;0 0 0 1];
+FocalMatrix = [foclength 0 0 0; 0 foclength 0 0;0 0 1 0];
+Orientation = getfield(ParamOneObject.Parameters(1,1),'orientation');
+Pmat = RMatrix * SMatrix;
+Kmat = KmatrixOne * FocalMatrix;
 WorldtoCamMatrix = KmatrixOne * FocalMatrix * RMatrix * SMatrix;
 mocapData = matfile('mocapPoints3D.mat');
 mocapPosition = mocapData.pts3D;
+
+
 
 CamOneCoord = zeros(3,39);
 
@@ -23,7 +26,9 @@ for i=1:N
     U = mocapPosition(1,i);
     V = mocapPosition(2,i);
     W = mocapPosition(3,i);
-    PixCoord = WorldtoCamMatrix * [U; V; W; 1;];
+    WorldCoord = [U; V; W; 1];
+    CamShift = WorldCoord - [positionOne(1); positionOne(2); positionOne(3); 0];
+    CamCoord = RMatrix * CamShift;
     CamOneCoord(1,i) = PixCoord(1);
     CamOneCoord(2,i) = PixCoord(2);
     CamOneCoord(3,i) = 1;
@@ -35,4 +40,4 @@ imshow(imOne);
 axis on;
 hold on;
 
-plot(CamOneCoord(1,1),CamOneCoord(2,1),'r+','MarkerSize',10)
+plot(u,v,'r+','MarkerSize',10)
